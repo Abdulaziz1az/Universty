7c2929d6fd5e10feb678eeebb0059f51e34b4b21
1234 <abc@gmail.com>
2025-01-21 12:21:27 -0600

    Declaring the variables

---
+++ Student.java
@@ -0,0 +1,11 @@
+// This class get the information from the students
+public class Student
+{
+    // Declaring the variables
+    String studentFirstName = "N/A";
+    String studentLastName = "N/A";
+    String major = "N/A";
+    int year = -1; 
+    int startId = -1;
+
+}
