bebf2f017d1ebd6c959778e3cdb41c18c01e24f1
1234 <abc@gmail.com>
2025-01-22 23:04:54 -0600

    I added all the gets

--- Student.java
+++ Student.java
@@ -6,6 +6,37 @@ public class Student
     String studentLastName = "N/A";
     String major = "N/A";
     int year = -1; 
-    int startId = -1;
+    String startId = "N/A";
 
+    // gets and sets 
+
+    // gets the first first name
+    public String getStudentFirstName()
+    {
+        return studentFirstName;
+    }
+
+    // gets the last name 
+    public String getStudentLastName()
+    {
+        return studentLastName;
+    }
+
+    // gets the major
+    public String getMajor()
+    {
+        return major;
+    }
+
+    // gets the year
+    public int getYear()
+    {
+        return year;
+    }
+
+    // gets the star id
+    public String getStarId()
+    {
+        return startId;
+    }
 }
