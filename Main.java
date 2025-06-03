295e0a52e0d153e8f415e971795624440a1316ff
Abdulaziz1az <AbdululazizAbdirisak15@gmail.com>
2025-06-02 22:09:30 -0500

    Abdulaziz Abdirisak 6/2/25 I added a Teacher class that has name, age, starid, and class.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-public class uuniversity {
+public class Main {
     public static void main(String[] args)
     {
-        
+
     }
 }
+
---
+++ Teacher.java
@@ -0,0 +1,27 @@
+public class Teacher {
+    private String name;
+    private int age;
+    private String StarId;
+    private String Class;
+
+    // getters, and setters
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String Aname)
+    {
+        this.name = Aname;
+    }
+    // Teachar Age
+    public int getAge()
+    {
+        return age;
+    }
+    public void setAge(int Aage)
+    {
+        this.age = Aage;
+    }
+}
+
